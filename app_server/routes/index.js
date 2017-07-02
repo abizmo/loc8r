d66ba3260ec4fa4e50124141a412726dee70ae47
abizmo <abizmo@gmail.com>
2017-07-02 09:46:58 +0100

    Switching from express routing to angular routing

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,15 +3,17 @@ var router = express.Router();
 var locations = require('../controllers/locations');
 var others = require('../controllers/others');
 
-// Locations URL
-router.get('/', locations.index);
-router.get('/locations/:locationId', locations.show);
-router.get('/locations/:locationId/reviews/new', locations.addReview);
-router.post('/locations/:locationId/reviews/new', locations.createReview);
-router.get('/locations/:locationId/times/new', locations.addTime);
-router.post('/locations/:locationId/times/new', locations.createTime);
+// // Locations URL
+// router.get('/', locations.index);
+// router.get('/locations/:locationId', locations.show);
+// router.get('/locations/:locationId/reviews/new', locations.addReview);
+// router.post('/locations/:locationId/reviews/new', locations.createReview);
+// router.get('/locations/:locationId/times/new', locations.addTime);
+// router.post('/locations/:locationId/times/new', locations.createTime);
+//
+// // Others URL
+// router.get('/about/', others.about);
 
-// Others URL
-router.get('/about/', others.about);
+router.get('/', others.angularApp);
 
 module.exports = router;
--- client/app.js
+++ client/app.js
@@ -1,2 +1,16 @@
 angular
-  .module('loc8rApp', []);
+  .module('loc8rApp', ['ngRoute']);
+
+function config($routeProvider) {
+  $routeProvider
+    .when('/', {
+
+    })
+    .others({
+      redirect('/')
+    });
+};
+
+angular
+  .module('loc8rApp')
+  .config(['$routeProvider', config]);
