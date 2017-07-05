23ce7e4283b3ced4527242c6fb238dd349cb49da
abizmo <abizmo@gmail.com>
2017-07-05 23:34:01 +0100

    Using controllerAs syntax

--- client/app.js
+++ client/app.js
@@ -5,7 +5,8 @@ function config($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'home/home.view.html',
-      controller: 'homeCtrl'
+      controller: 'homeCtrl',
+      controllerAs: 'vm'
     })
     .otherwise({
       redirectTo: '/'
