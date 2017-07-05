c88ce57e52127d7bc2c778748ab7d00b423bb4f9
abizmo <abizmo@gmail.com>
2017-07-05 22:56:39 +0100

    Creating an angular view

--- client/app.js
+++ client/app.js
@@ -4,10 +4,10 @@ angular
 function config($routeProvider) {
   $routeProvider
     .when('/', {
-
+      templateUrl: 'home/home.view.html'
     })
-    .others({
-      redirect('/')
+    .otherwise({
+      redirectTo: '/'
     });
 };
 
