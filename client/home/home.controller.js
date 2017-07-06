038fb664ad13f910e9ab45363581fe243673c458
abizmo <abizmo@gmail.com>
2017-07-06 23:02:33 +0100

    Using services

--- client/home/home.controller.js
+++ client/home/home.controller.js
@@ -2,7 +2,7 @@ angular
   .module('loc8rApp')
   .controller('homeCtrl', homeCtrl);
 
-function homeCtrl() {
+function homeCtrl($scope, loc8rData, geolocation) {
   var vm = this;
   vm.pageHeader = {
     title: 'Loc8r',
@@ -11,4 +11,35 @@ function homeCtrl() {
   vm.sidebar = {
     content: "Looking for wifi and seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
   };
+  vm.message = "Checking for your location!";
+
+  vm.getData = function (position) {
+    vm.message = "Searching...";
+    var lng = position.coords.longitude;
+    var lat = position.coords.latitude;
+
+    loc8rData.locationByCoords(lng, lat)
+      .then(function(response) {
+        vm.message = response.data.length > 0 ? "" : "No results";
+        vm.data = { locations: response.data };
+      })
+      .catch(function (e) {
+        vm.message = "Something happens!!";
+        console.log(e);
+      });
+  };
+
+  vm.showError = function (err) {
+    $scope.$apply(function () {
+      vm.message = err.message;
+    });
+  };
+
+  vm.noGeo = function () {
+    $scope.$apply(function () {
+      vm.message = "Your browser doesn't support GeoLocation!";
+    });
+  };
+
+  geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
 };
