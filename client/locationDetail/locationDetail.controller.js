04dcbd1b8f0467c7cd2d36ad0f59475ab4d544a7
abizmo <abizmo@gmail.com>
2017-07-13 22:56:46 +0100

    Using URL parameters in controllers and services

--- client/locationDetail/locationDetail.controller.js
+++ client/locationDetail/locationDetail.controller.js
@@ -1,14 +1,21 @@
 (function () {
   angular
     .module('loc8rApp')
-    .controller('locationDetailCtrl', ['$routeParams', locationDetailCtrl]);
+    .controller('locationDetailCtrl', ['$routeParams', 'loc8rData', locationDetailCtrl]);
 
-  function locationDetailCtrl($routeParams) {
+  function locationDetailCtrl($routeParams, loc8rData) {
     var vm = this;
     var locationId = $routeParams.locationId;
 
-    vm.pageHeader = {
-      title: locationId
-    };
+    loc8rData.locationById(locationId)
+      .then(function (response) {
+        vm.location = response.data;
+        vm.pageHeader = {
+          title: vm.location.name
+        };
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   };
 })();
