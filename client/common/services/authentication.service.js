0d8b49df446ac0f8a4d12620f676d79942adbc46
abizmo <abizmo@gmail.com>
2017-08-02 22:06:01 +0100

    Allowing users to sign up, sign in and sign out

--- client/common/services/authentication.service.js
+++ client/common/services/authentication.service.js
@@ -12,9 +12,30 @@
       return $window.localStorage['loc8r-token'];
     };
 
+    function register(user) {
+      return http.post('/api/register', user)
+        .then(function (data) {
+          saveToken(data.token);
+        });
+    };
+
+    function login(user) {
+      return http.post('/api/login', user)
+        .then(function (data) {
+          saveToken(data.token);
+        });
+    };
+
+    function logout(user) {
+      $window.localStorage.removeItem('loc8r-token');
+    };
+
     return {
       saveToken: saveToken,
-      getToken: getToken
+      getToken: getToken,
+      register: register,
+      login: login,
+      logout: logout
     };
   };
 })();
