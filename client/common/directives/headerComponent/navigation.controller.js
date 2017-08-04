f5788739dd7ca0efedb5fcc6a2ecf6ec10cbb322
abizmo <abizmo@gmail.com>
2017-08-04 21:43:33 +0100

    Updating navigation

--- client/common/directives/headerComponent/navigation.controller.js
+++ client/common/directives/headerComponent/navigation.controller.js
@@ -1,13 +1,20 @@
 (function () {
   angular
     .module('loc8rApp')
-    .controller('navigationCtrl', ['$location', navigationCtrl]);
+    .controller('navigationCtrl', ['$location', 'authentication',navigationCtrl]);
 
-  function navigationCtrl($location) {
+  function navigationCtrl($location, authentication) {
     var vm = this;
 
     vm.currentPath = $location.path();
 
-    console.log($location.path());
+    vm.isLoggedIn = authentication.isLoggedIn();
+
+    vm.currentUser = authentication.currentUser();
+    
+    vm.logout = function () {
+      authentication.logout();
+      $location.path('/');
+    };
   };
 })();
